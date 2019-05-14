a7ed71cfa96983dd1cb8fe2d55f043c63ae3d7b2
Tam O'Donnell <thomashodonnell@gmail.com>
2019-05-14 18:19:24 +0100

    dropdown showing families names

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 const instrumentFamilyData = require('./data/instrument_family_data.js')
+const InstrumentFamilies = require('./models/instrument_families.js')
+const SelectView = require('./views/select_view.js')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+
+  const selectElement = document.querySelector('select#instrument-families')
+  const instrumentFamiliesDropdown = new SelectView(selectElement);
+  instrumentFamiliesDropdown.bindEvents();
+
+  const instrumentFamilies = new InstrumentFamilies(instrumentFamilyData);
+  instrumentFamilies.bindEvents();
+
+
+
+
 });
---
+++ src/views/select_view.js
@@ -0,0 +1,28 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+    const SelectView = function (element) {
+        this.element = element;
+    };
+
+    SelectView.prototype.bindEvents = function () {
+        PubSub.subscribe('Families:options-ready', (event) => {
+            const allFamilies = event.detail;
+            this.populate(allFamilies);
+        });
+
+        this.element.addEventListener('change', (event) => {
+            const selectedFamily = event.target.value;
+            PubSub.publish('SelectView:change', selectedFamily);
+        });
+    };
+
+    SelectView.prototype.populate = function (instrumentFamilyData) {
+        instrumentFamilyData.forEach((family, index) => {
+            const option = document.createElement('option');
+            option.textContent = family.name;
+            option.value = index;
+            this.element.appendChild(option);
+        })
+    }
+
+    module.exports = SelectView;
