5e5c5d40d154862289165fe603ffc9c2d1cd6123
Tam O'Donnell <thomashodonnell@gmail.com>
2019-05-14 16:38:01 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const instrumentFamilyData = require('./data/instrument_family_data.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+});
---
+++ src/models/instrument_families.js
@@ -0,0 +1,7 @@
+
+
+const InstrumentFamilies = function(data) {
+  this.data = data;
+};
+
+module.exports = InstrumentFamilies;
